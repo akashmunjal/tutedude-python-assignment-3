836540b8851aba3d8b719cfda8ff8b5c45442cd5
akashmunjal <50265641+akashmunjal@users.noreply.github.com>
2025-05-07 19:28:30 +0530

    Add files via upload

---
+++ Assignment 3_Task 1.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n<2 :
+        return 1
+    else:
+        return(n*factorial(n-1))
+
+x = int(input("enter a number: "))
+print("Factorial of ",x," is ", factorial(x))
+
---
+++ Assignment 3_Task 2.py
@@ -0,0 +1,6 @@
+x = float(input("Enter a number: "))
+
+import math 
+print("Square root: ", math.sqrt(x))
+print("Logarithm: ", math.log(x))
+print("Sine: ", math.sin(x))
